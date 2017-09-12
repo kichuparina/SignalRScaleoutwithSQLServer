9a7cf800d9997b365be54d7d1b57c27c1bd6d867
Beyond <this.esty@live.com>
2017-09-12 15:32:59 +0600

    Enable Cors

--- SignalRPushSQLServer/App_Start/WebApiConfig.cs
+++ SignalRPushSQLServer/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace SignalRPushSQLServer
 {
@@ -13,7 +14,8 @@ namespace SignalRPushSQLServer
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-            config.EnableCors();
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{action}/{id}",
